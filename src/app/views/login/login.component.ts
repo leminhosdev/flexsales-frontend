a4cafd1a1391818dec816bbff5d25e40a765d4f0
leminhosdev <lucaslemos8294@gmail.com>
2023-07-20 19:21:43 -0300

    login form valdiated

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  form!: FormGroup;
+    
+  constructor (private formBuilder: FormBuilder) {
+    
+  }
 
+  ngOnInit(): void {
+   this.form = this.formBuilder.group({
+    
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+     
+   })
+  
+  }
+
+ 
+  get email(){
+    return this.form.get('email')!;
+  }
+  get password(){
+    return this.form.get('password')!;
+  }
+  
+
+
+
+  onRegisterSubmit() {
+    if(this.form.invalid){
+      return;
+    }
+    console.log('Formulário de Registro enviado!');
+    
+  }
 }
