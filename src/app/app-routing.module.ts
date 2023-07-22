7c6d35f05f6023b9ec1130cdb00d7c9f4c0336b7
leminhosdev <lucaslemos8294@gmail.com>
2023-07-22 18:30:33 -0300

    new screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,8 +20,7 @@ const routes: Routes = [
     },
     {
       path: 'initial',
-      component: InitialComponent,
-      canActivate: [AuthguardGuard]
+      component: InitialComponent
 
     }
 ];
