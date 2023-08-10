79b8cf98f3c3911748ecd3bb36e352669fcd039b
leminhosdev <lucaslemos8294@gmail.com>
2023-08-10 09:28:03 -0300

    working datas on chart

--- src/app/views/initial/initial.component.ts
+++ src/app/views/initial/initial.component.ts
@@ -6,6 +6,7 @@ import { ExcelFile } from 'src/app/entitys/ExcelFileModel';
 import { ClientServiceService } from 'src/app/services/client-service.service';
 import { ProductComponent } from '../product/product.component';
 import { Chart, registerables  } from 'chart.js'
+import { OrderEntity } from 'src/app/entitys/OrderEntityModel';
 Chart.register(...registerables)
 @Component({
   selector: 'app-initial',
@@ -18,7 +19,7 @@ export class InitialComponent {
     user!: Client;
     file!: File;
     client!: Client;
-   
+   listOrder: OrderEntity[] = []
 
   constructor (private formBuilder: FormBuilder, private clientService: ClientServiceService,  public dialog: MatDialog) {
    
@@ -33,43 +34,75 @@ export class InitialComponent {
      file:['']
    
    })
+   
    this.getLoggedUser()
-
-   var myChart = new Chart("piechart", {
-    type: 'line',
-    data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dec' ],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3, 22, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});  
+   this.dads(8)
   }
+  createChart(value: number){
+    var myChart = new Chart("piechart", {
+      type: 'line',
+      data: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dec' ],
+          datasets: [{
+              label: '# of Votes',
+              data: [22 , 20 , 1029, 3, 5, 2, 3, value, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });  
+  }
+
+  public dads(selectedMonth: number){
+    var value = 0;
+    
+    this.clientService.getLoggedUser().subscribe(
+      userlogged => {
+       
+        
+        userlogged.orderEntityList.forEach(order => {
+          const dateString = order.salesData
+          const dateObject = new Date(dateString);
+         
+
+         
+          const orderMonth = dateObject.getMonth() + 1;
+          
+          console.log(order);
+          if (orderMonth === selectedMonth) {
+            value += order.totalPrice;
+          }
+        });
+        this.createChart(value)
+      }
+    ); 
+      
+    
+   
+  }
+  
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ProductComponent, {
@@ -88,6 +121,7 @@ export class InitialComponent {
     userlogged => {
       this.client = userlogged;
     
+      return userlogged;
     }
   ); 
   }
