4efb2701089f8400808801315357401d9bb6e51d
leminhosdev <lucaslemos8294@gmail.com>
2023-08-10 11:43:15 -0300

    all charts done

--- src/app/views/initial/initial.component.ts
+++ src/app/views/initial/initial.component.ts
@@ -7,6 +7,7 @@ import { ClientServiceService } from 'src/app/services/client-service.service';
 import { ProductComponent } from '../product/product.component';
 import { Chart, registerables  } from 'chart.js'
 import { OrderEntity } from 'src/app/entitys/OrderEntityModel';
+import { toPercentage } from 'chart.js/dist/helpers/helpers.core';
 Chart.register(...registerables)
 @Component({
   selector: 'app-initial',
@@ -37,33 +38,113 @@ export class InitialComponent {
    
    this.getLoggedUser()
    this.dads(8)
+   
   }
-  createChart(value: number){
+  createChart(monthlyValues: number[], monthlyProfit: number[] ){
     var myChart = new Chart("piechart", {
+      type: 'bar',
+      data: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dec' ],
+          datasets: [{
+              label: 'Revenue',
+              data: monthlyValues,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.3)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+              ],
+              borderWidth: 1
+          },
+          {
+            label: 'Products',
+              data: monthlyProfit,
+              backgroundColor: [
+                  'rgba(15, 129, 2, 0.3)'
+              ],
+              borderColor: [
+                  'rgba(15, 129, 2, 1)',
+              ],
+              borderWidth: 1
+          }
+        
+        ]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });  
+  }
+  createChart3(monthlyProfit: number[], monthlyProduct: number[], monthlyTaxes: number[] ){
+    var myChart = new Chart("piechart3", {
       type: 'line',
       data: {
           labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dec' ],
           datasets: [{
-              label: '# of Votes',
-              data: [22 , 20 , 1029, 3, 5, 2, 3, value, 19, 3, 5, 2, 3],
+              label: 'Taxes',
+              data: monthlyTaxes,
               backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
+                  'rgb(255, 0, 0, 0.3)'
               ],
               borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
+                  'rgb(255, 0, 0, 1)',
               ],
               borderWidth: 1
-          }]
+          },
+          {
+            label: 'Profit',
+              data: monthlyProfit,
+              backgroundColor: [
+                  'rgb(0, 0, 255, 0.3)'
+              ],
+              borderColor: [
+                  'rgb(0, 0, 255)',
+              ],
+              borderWidth: 1
+          },
+          {
+            label: 'Sales',
+              data: monthlyProduct,
+              backgroundColor: [
+                  'rgb(255, 165, 0, 0.3)'
+              ],
+              borderColor: [
+                  'rgb(255, 165, 0, 1)',
+              ],
+              borderWidth: 1
+          }
+        
+        ]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });  
+  }
+  createChart2(totalRevenue: number, totalProfit: number, totalTaxes:number){
+    var myChart = new Chart("piechart2", {
+      type: 'doughnut',
+      data: {
+          labels: [ 'Profit', 'Taxes'],
+          datasets: [{
+              label: 'Datas',
+              data: [ totalProfit, totalTaxes],
+              backgroundColor: [
+                  'rgba(65, 59, 102, 1)',
+                  'rgba(165, 89, 182, 1)',   
+                  'rgba(125, 39, 62, 1)'          
+              ],
+              borderWidth: 1
+          }
+        ]
       },
       options: {
           scales: {
@@ -75,27 +156,47 @@ export class InitialComponent {
   });  
   }
 
+  
+
   public dads(selectedMonth: number){
     var value = 0;
+    var totalRevenue = 0;
+    var totalProfit = 0;
+    var totalTaxes = 0;
     
     this.clientService.getLoggedUser().subscribe(
       userlogged => {
        
-        
+        const monthlyProduct = new Array<number>(12).fill(0);
+        const monthlyValues = new Array<number>(12).fill(0);
+        const monthlyProfit = new Array<number>(12).fill(0);
+        const monthlyTaxes = new Array<number>(12).fill(0);
+        const datas: number[] = [];    
         userlogged.orderEntityList.forEach(order => {
-          const dateString = order.salesData
-          const dateObject = new Date(dateString);
-         
-
-         
-          const orderMonth = dateObject.getMonth() + 1;
-          
-          console.log(order);
-          if (orderMonth === selectedMonth) {
-            value += order.totalPrice;
-          }
-        });
-        this.createChart(value)
+          totalRevenue += order.totalPrice;
+          totalProfit += order.totalCommissionValue;
+          totalTaxes += order.totalTaxesValue;
+        })
+        datas.push(totalRevenue, totalProfit, totalTaxes);
+        for (let month = 0; month < 12; month++) {
+          userlogged.orderEntityList.forEach(order => {
+            const dateString = order.salesData;
+            const dateObject = new Date(dateString);
+            const orderMonth = dateObject.getMonth();
+            
+           
+            
+            if (orderMonth === month) {
+              monthlyValues[month] += order.totalPrice;
+              monthlyProfit[month] += order.totalCommissionValue
+              monthlyProduct[month] += order.productsAmount
+              monthlyTaxes[month] += order.totalTaxesValue
+            }
+          });
+        }
+        this.createChart(monthlyValues, monthlyProfit )
+        this.createChart2(totalRevenue, totalProfit, totalTaxes)
+        this.createChart3(monthlyProfit, monthlyProduct, monthlyTaxes)
       }
     ); 
       
